63c3e3e2615405e224b76a14e514f2b700c23b95
GabrielZ1 <GabrielZ1@localhost>
2023-04-07 18:15:41 -0700

    Finished: strings_cannot_add, rollercoaster, voting_booth. Spot the error: fixtherandom, howmany, and methodcall.

--- src/_04_spot_the_error/FixTheRandom.java
+++ src/_04_spot_the_error/FixTheRandom.java
@@ -7,13 +7,13 @@ public class FixTheRandom {
 	// This program is supposed to get a random number between 5 and 10 and print a different message for each number.
 	// BUT it's not working. If you don't believe me, try running it.
 	// Change ONE line of code to fix this program.
-	
+
 	public static void main(String[] args) {
-		
+
 		Random random = new Random();
-		
-		int numnber = random.nextInt(5);
-		
+
+		int numnber = random.nextInt(10-5+1) + 5;  //i changed this line from "int numnber = random.nextInt(5);" to what it is now
+
 		if (numnber == 5) {
 			System.out.println("FIVE");
 		} else if (numnber == 6) {
@@ -28,6 +28,6 @@ public class FixTheRandom {
 			System.out.println("TEN");
 		}
 	}
-	
-	
+
+
 }
--- src/_04_spot_the_error/HowMany.java
+++ src/_04_spot_the_error/HowMany.java
@@ -3,19 +3,21 @@ package _04_spot_the_error;
 import java.util.Random;
 
 public class HowMany {
-	
+
 	// This program is supposed to print a random number between 1 and 100.
 	// BUT it's not working. If you don't believe me, try running it.
 	// Change one line of code to fix this program.
-	
+
+
 	public static void main(String[] args) {
-		int number = 0;
+
+		int number = getRandomNumber(); //i added this line
 		System.out.println(number);
 	}
-	
+
 	public static int getRandomNumber() {
 		Random random = new Random();
-		int number = random.nextInt(100)+1;
+		int number = random.nextInt(100) + 1;    //this is the same as "int number = random.nextInt(100-1+1) + 1;" [the -1 and +1 cancel out]
 		return number;
 	}
 }
